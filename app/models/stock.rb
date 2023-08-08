91a6ff4efa09cec5ead12d929406cf57b1b2bd8d
Bilal <ranabilalrajput09@gmail.com>
2023-08-08 09:36:42 +0200

    Add code for my_portfolio page and partial

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,10 @@ class Stock < ApplicationRecord
         publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
         endpoint: 'https://cloud.iexapis.com/v1'
         )
-        client.price(ticker_symbol)
+        begin
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+            return nil
+        end
     end
 end
