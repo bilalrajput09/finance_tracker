f203acc21772c1a387b69dacf10996a52e5aecf0
Bilal <ranabilalrajput09@gmail.com>
2023-08-08 09:42:18 +0200

    Add assosiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
