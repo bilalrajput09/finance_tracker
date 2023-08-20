07f91d4ad39435d22e317071d9bb306945897268
Bilal <ranabilalrajput09@gmail.com>
2023-08-20 10:44:49 +0200

    Add full_name feature to users profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
