c4e7d9b30cec5b11e46fac99bba9478d9d3503e4
Bilal <ranabilalrajput09@gmail.com>
2023-08-21 09:17:55 +0200

    Add my_friend functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    @friends_list = current_user.friends
+    @name = params[:friend].capitalize
+    @friends = @friends_list.select { |friend| @name == friend.first_name }
+        if @name.present?
+            if @name
+                render 'users/my_friends'
+            else
+                flash[:alert] = "Please enter a valid name."
+                redirect_to my_friends_path
+            end
+        else
+            flash[:alert] = "Please enter a name to search"
+            redirect_to my_friends_path
+        end
+    end
 end
